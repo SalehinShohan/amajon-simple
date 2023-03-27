6edb0af5fa3d11c23283b5f009996338055354f0
SalehinShohan <ps.shohan1@gmail.com>
2023-03-28 01:00:43 +0600

    added some card in loacal storbage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -12,11 +13,33 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data));
 
-    }, [])
+    }, []);
 
-    const handleAddToCart = (product) =>{
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        //step 1: get the id
+        for (const id in storedCart) {
+            //step 2: get the products by using id
+            const savedProduct = products.find(product => product.id === id);
+            if (savedProduct) {
+                //step 3: get the quantity of the product
+                const quantity = storedCart[id];
+                savedProduct.quantity = quantity;
+                //step 4: add the added product to the saved cart
+                savedCart.push(savedProduct);
+            }
+            // console.log(savedProduct);
+
+        }
+        //step 5: set the cart
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
@@ -26,7 +49,7 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
-                        handleAddToCart = {handleAddToCart}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
